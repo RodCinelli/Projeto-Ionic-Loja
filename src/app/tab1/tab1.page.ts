4c321e2c3d34b8dae5f4d11d8309ad944ae6f695
Rodrigo Cinelli <rodcinelli@gmail.com>
2023-12-11 04:14:54 -0300

    Corrigindo erros da página inicial.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,28 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, QueryList, ViewChildren, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit {
+export class Tab1Page implements AfterViewInit {
+  @ViewChildren('galleryImage') images!: QueryList<ElementRef>;
   currentImageIndex: number = 0;
-  images: NodeListOf<HTMLImageElement> = null!; // Inicialização defensiva
   likes: number = 0;
 
+  constructor() {}
 
-  ngOnInit() {
-    this.images = document.querySelectorAll('.gallery-image');
-    this.images[this.currentImageIndex].style.display = 'block'; // Mostrar a primeira imagem
+  ngAfterViewInit() {
+    // Inicializar a exibição da primeira imagem após a view ser inicializada.
+    const imagesArray = this.images.toArray();
+    // Definir todas as imagens para display: none
+    imagesArray.forEach(image => (image.nativeElement.style.display = 'none'));
+    // Exceto a primeira imagem.
+    if (imagesArray.length) {
+      imagesArray[this.currentImageIndex].nativeElement.style.display = 'block';
+    }
   }
 
+  // Navegar para a próxima imagem
   nextImage() {
-    this.images[this.currentImageIndex].style.display = 'none'; // Ocultar imagem atual
-    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
-    this.images[this.currentImageIndex].style.display = 'block'; // Mostrar próxima imagem
+    this.changeImage((this.currentImageIndex + 1) % this.images.length);
   }
 
+  // Navegar para a imagem anterior
+  previousImage() {
+    this.changeImage((this.currentImageIndex - 1 + this.images.length) % this.images.length);
+  }
+
+  // Mostrar uma imagem específica com base no índice
+  showImage(index: number) {
+    this.changeImage(index);
+  }
+
+  // Alterar a imagem visível
+  private changeImage(newIndex: number) {
+    const imagesArray = this.images.toArray();
+    imagesArray[this.currentImageIndex].nativeElement.style.display = 'none';
+    this.currentImageIndex = newIndex;
+    imagesArray[this.currentImageIndex].nativeElement.style.display = 'block';
+  }
+
+  // Incrementar o contador de curtidas
   addlikes() {
-    this.likes++; // Incrementar likes
+    this.likes++;
   }
 }
